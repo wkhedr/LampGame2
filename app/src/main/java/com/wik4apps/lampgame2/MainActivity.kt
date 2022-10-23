2d713fffeaf1bee3e1cfc442975d4d72a2435b36
wkhedr <khedrw@gmail.com>
2022-10-23 12:05:31 +0200

    Initial commit

---
+++ app/src/main/java/com/wik4apps/lampgame2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.wik4apps.lampgame2
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
